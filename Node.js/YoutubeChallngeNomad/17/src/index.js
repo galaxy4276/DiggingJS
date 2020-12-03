0ac48e7ff33e3d67d209a381e5ff12fd1c4f0e82
galaxy <galaxyhi4276@gmail.com>
2020-12-04 05:25:27 +0900

    challenge: index 페이지 작성

--- Node.js/YoutubeChallngeNomad/17/src/index.js
+++ Node.js/YoutubeChallngeNomad/17/src/index.js
@@ -5,13 +5,25 @@ import path from "path";
 import bodyParser from "body-parser";
 import movieRouter from "./movieRouter";
 import { localsMiddleware } from "./middlewares";
+import logger from 'morgan';
 
 const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(localsMiddleware);
 app.use("/", movieRouter);
 
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  err.message = '😥😥😥😥 Cannot Find Page 😥😥😥😥';
+  res.render('404', { err });
+});
 // Codesanbox does not need PORT :)
-app.listen(() => console.log(`✅  Server Ready!`));
+app.listen(8001, () => console.log(`✅  Server Ready!`));
